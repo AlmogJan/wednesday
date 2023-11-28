e37a155cc8788d358ac9406e10d9a54f0fa77d73
AlmogJan <Almogj1998@gmail.com>
2023-11-28 13:50:20 +0200

    init

---
+++ src/Index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import './styles/main.scss'
+import { RootCmp } from './RootCmp'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RootCmp></RootCmp>
+)
---
+++ src/RootCmp.tsx
@@ -0,0 +1,5 @@
+export function RootCmp() {
+  return <div>
+    <h1>hi</h1>
+  </div>
+}
